842c905dc28f53149a10f49661b0eff826becfb6
aFbbb <eliott.legru@gmail.com>
2025-06-19 22:24:15 +0200

    Almost release

--- js/videohover.js
+++ js/videohover.js
@@ -1,16 +1,22 @@
-  
-document.querySelectorAll('.element').forEach(el => {
-  const img   = el.querySelector('.imgCI');
-  const video = el.querySelector('.videoCI');
-  img.addEventListener('mouseenter', () => {
-    video.currentTime = 0;
-    video.play();
-    video.style.opacity = 1;
-    img.style.opacity = 0;
-  });
-  img.addEventListener('mouseleave', () => {
-     video.pause();
-     video.style.opacity = 0;
-    img.style.opacity = 1;
+// js/videoHover.js
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.element').forEach(el => {
+    const img   = el.querySelector('.imgCI');
+    const video = el.querySelector('.videoCI');
+
+    el.addEventListener('mouseenter', () => {
+      video.currentTime = 0;
+      video.play();
+      video.style.opacity = 1;
+      img.style.opacity = 0;
+      img.style.pointerEvents = 'none'; // pour laisser passer la souris vers la vidéo
+    });
+
+    el.addEventListener('mouseleave', () => {
+      video.pause();
+      video.style.opacity = 0;
+      img.style.opacity = 1;
+      img.style.pointerEvents = '';    // restore
+    });
   });
 });
